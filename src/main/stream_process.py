e4dc241c472a3e1649be8a3039a7909a3faec7ee
alireza <sdamoosavi@gmail.com>
2020-10-24 10:32:17 +0330

    streaming is debuged

--- src/main/stream_process.py
+++ src/main/stream_process.py
@@ -1,12 +1,15 @@
 import os, math
 from functools import partial
 
+from functools import reduce
+from pyspark.sql import DataFrame
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import expr, split
 from pyspark.sql.functions import from_json
 from pyspark.sql.types import *
 from pyspark.storagelevel import StorageLevel
-from pyspark.sql.functions import col, udf
+from pyspark.sql.functions import col, udf, when, lag, lit, isnull
+from pyspark.sql.window import Window
 
 url = os.getenv("db_url")
 
@@ -39,12 +42,16 @@ def get_street_section(lat):
 udfStreet = udf(get_street_section, StringType())
 
 
-def calc_speed(lat1, lng1, time1, lat2, lng2, time2):
-    A = 69.1 * (lat2 - lat1)
-    B = 69.1 * (lng2 - lng1) * math.cos(lat1 / 57.3)
+def calc_speed(lat, d_lat, d_lng, time, prev_time):
+
+    A = 69.1 * d_lat
+    B = 69.1 * d_lng * math.cos(lat / 57.3)
     d = ((A ** 2 + B ** 2) ** 0.5) * 1609.344
-    t = time2 - time1
-    return (d / t.seconds) * 3.6
+    d_time = time - prev_time
+    return (d / d_time.seconds) * 3.6
+
+
+udfVelocity = udf(calc_speed, FloatType())
 
 
 def apply_location_requirement(df):
@@ -56,6 +63,50 @@ def apply_location_requirement(df):
 
     df = df.withColumn("street", udfStreet("lat"))
     df = df.withColumn("street", df["street"].cast("string").alias("street"))
+
+    w = Window().partitionBy("driverId").orderBy("time")
+    distinct_driverId = df.select("driverId").distinct()
+    _dfs = []
+
+    for driver_id in distinct_driverId:
+        ndf = df.filter(df.driverId == driver_id)
+        ndf = ndf.withColumn("prev_lng", lag("lng", 1).over(w))
+        ndf = ndf.withColumn(
+            "prev_lng", ndf["prev_lng"].cast("float").alias("prev_lng")
+        )
+
+        ndf = ndf.withColumn("prev_lat", lag("lat", 1).over(w))
+        ndf = ndf.withColumn(
+            "prev_lat", ndf["prev_lat"].cast("float").alias("prev_lat")
+        )
+
+        ndf = ndf.withColumn("prev_time", lag("time", 1).over(w))
+
+        ndf = ndf.withColumn(
+            "prev_time", ndf["prev_time"].cast("timestamp").alias("prev_time")
+        )
+
+        ndf = ndf.withColumn(
+            "d_lng",
+            when(col("prev_lng").isNotNull(), col("lng") - col("prev_lng")).otherwise(
+                0
+            ),
+        )
+        ndf = ndf.withColumn(
+            "d_lat",
+            when(col("prev_lat").isNotNull(), col("lat") - col("prev_lat")).otherwise(
+                0
+            ),
+        )
+
+        _dfs.append(ndf)
+
+    df = reduce(DataFrame.unionAll, _dfs)
+
+    df = df.withColumn(
+        "velocity", udfVelocity("lat", "d_lat", "d_lng", "time", "prev_time")
+    )
+    df = df.withColumn("velocity", df["velocity"].cast("float").alias("velocity"))
 
     return df
 
@@ -98,17 +149,15 @@ def main():
     # sdf_locations_data = sdf_locations_data.withWatermark("time", "15 minutes")
 
     sdf_locations_data.printSchema()
-    # sdf_locations_data.writeStream.format("console").start().awaitTermination()
 
     # query = sdf_locations_data.groupBy("driverId").count()
+    query = sdf_locations_data.filter(sdf_locations_data.driverId == 133)
 
-    # query.writeStream.outputMode("complete").format("console").option(
-    #     "truncate", False
-    # ).start().awaitTermination()
+    query.writeStream.format("console").start().awaitTermination()
 
-    sdf_locations_data.writeStream.outputMode("append").format("csv").foreachBatch(
-        partial(postgres_sink, table_name="locations")
-    ).trigger(processingTime="10 minutes").start().awaitTermination()
+    # sdf_locations_data.writeStream.outputMode("append").format("csv").foreachBatch(
+    #     partial(postgres_sink, table_name="locations")
+    # ).trigger(processingTime="10 minutes").start().awaitTermination()
 
 
 if __name__ == "__main__":
